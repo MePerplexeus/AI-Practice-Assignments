dc15b084f3e1887bd95ad8e14269c1d85d46999d
Prithvi Soma <74184797+MePerplexeus@users.noreply.github.com>
2020-12-05 23:26:48 +0530

    Add files via upload

---
+++ 2020_10_31_CodingChallenge1/compound_interest.py
@@ -0,0 +1,53 @@
+# Coding Challenge 1
+# Name:
+# Student No:
+
+# A Simple and Compound Interest Calulator
+
+def print_intro():
+    pass
+
+
+def get_input():
+    pass
+
+
+def simple_interest(principal, rate, years):
+    pass
+
+
+def compound_interest(principal, rate, years):
+    pass
+
+
+def print_simple_output(principal, rate, years, result):
+    pass
+
+
+def print_compounding_output(principal, rate, years, result):
+    pass
+
+
+# ---------- Challenge Functions (Optional) ----------
+
+def print_target_output(principal, rate, years):
+    pass
+
+def get_target_input():
+    pass
+
+def calculate_years_to_target(principal, rate, target):
+    pass
+
+# ---------- Main driver function ----------
+# 1.	Print a welcome message explaining the purpose of the program.
+# 2.	Prompt the user for the necessary inputs (see formulae and brief) and convert the values the user enters into suitable data types.
+# 3.	Perform the simple and compound interest calculations.
+# 4.	Print the results to the terminal in the format specified.
+def main():
+    pass
+
+
+# Program execution begins here
+if __name__ == '__main__':
+    main()
